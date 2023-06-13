ebed26b7edc071874181ed83b75d9f4134876db5
Iremel Iskuzhin <9588504@gmail.com>
2023-06-13 23:40:27 +0500

    Bot: fix commands, add server, add dockerfiles

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,8 @@ from telegram import __version__ as TG_VER
 from telegram import ForceReply, Update
 from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+# from telegram.helpers import escape_markdown
+# from telegram.utils.helpers import escape_markdown
 import requests
 # import openai
 import re
@@ -44,13 +46,32 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text("Помощь")
 
 
+# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     try:
+#         response = send_request(update.message.text)
+#         escaped_text = escape_chars(response)
+#         await update.message.reply_text(escaped_text, parse_mode=ParseMode.MARKDOWN_V2)
+#     except:
+#         await update.message.reply_text('Что то пошло не так, повторите вопрос.')
+#
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
-        response = send_request(update.message.text)
-        escaped_text = escape_chars(response)
+        response = send_api(update.message.text)
+        escaped_text = escape_chars(response['message'])
         await update.message.reply_text(escaped_text, parse_mode=ParseMode.MARKDOWN_V2)
-    except:
+    except Exception as error:
+        print(error.args)
         await update.message.reply_text('Что то пошло не так, повторите вопрос.')
+
+
+def send_api(message):
+    url = env['SERVER_URL'] + '/bot'
+    headers = {"Content-Type": "application/json"}
+    # data = {"messages": message}
+    response = requests.post(url, headers=headers, json=message)
+    print(response.json())
+    return response.json()
 
 
 def escape_chars(text):
@@ -59,36 +80,24 @@ def escape_chars(text):
     return re.sub(f"([{re.escape(pattern)}])", r"\\\1", text)
 
 
-def send_request(prompt, model="gpt-3.5-turbo", max_tokens=60):
-    # # model = "text-davinci-002"
-    # max_tokens = 1024
-    # prompt = "Write a short story about a man who finds a mysterious key in his garden."
-    # response = openai.Completion.create(
-    #   engine=model,
-    #   prompt=prompt,
-    #   max_tokens=max_tokens,
-    #   n=1,
-    #   stop=None,
-    #   temperature=0.5,
-    # )
-    # text = response.choices[0].text.strip()
-    # return text
-    url = env['GPT_URL']
-    headers = {"Authorization": "Bearer " + env["GPT_TOKEN"], "Content-Type": "application/json"}
-    data = {"model": model, "messages": [{"role": "user", "content": prompt}]}
-    response = requests.post(url, headers=headers, json=data)
-    print(response.json())
-    return response.json()['choices'][0]['message']['content']
+# def send_request(prompt, model="gpt-3.5-turbo", max_tokens=60):
+#     url = env['GPT_URL']
+#     headers = {"Authorization": "Bearer " + env["GPT_TOKEN"], "Content-Type": "application/json"}
+#     data = {"model": model, "messages": [{"role": "user", "content": prompt}]}
+#     response = requests.post(url, headers=headers, json=data)
+#     print(response.json())
+#     return response.json()['choices'][0]['message']['content']
 
 
 def main() -> None:
-    application = Application.builder().token(env["BOT_TOKEN"]).build()
+    # application = Application.builder().token(env["BOT_TOKEN"]).read_timeout(30).write_timeout(30).build()
+    application = Application.builder().token(env["BOT_TOKEN"]).read_timeout(60).get_updates_read_timeout(60).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    # application.add_handler(CommandHandler("api", api_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.run_polling()
 
 
 if __name__ == "__main__":
     main()
-
