7ff815fd6827ecc010da6e9bbbbd5e3ef5e114db
Iremel Iskuzhin <9588504@gmail.com>
2023-06-16 22:57:36 +0500

    add envs to workflows

--- server/test_app.py
+++ server/test_app.py
@@ -1,10 +1,12 @@
 from fastapi.testclient import TestClient
 from app import app
-import settings
+
 
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+    
